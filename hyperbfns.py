fbdfdb022ae7445c6f99e997abc54a5beb86c403
Steven Dorsher <sdorsher@gmail.com>
2020-10-24 22:09:46 -0500

    First attempt at hyperbolic orbits.

---
+++ hyperbfns.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def getelltheory(theta0,ecc0,rad0):
+    ell=rad0*(ecc0+1.)
+    return ell
+
+def getrtheory(theta0,ecc0,rad0,t):
+
+    ell=getelltheory(theta0,ecc0,rad0)
+    r=ell/(1+ecc0*np.cos(t))
+    return r
+
+def getxtheory(theta0,ecc0,rad0,t,sgn):
+    r=getrtheory(theta0,ecc0,rad0,t)
+    x=sgn*(np.cos(t)*r)
+    return x
+
+def getytheory(theta0,ecc0,rad0,t,sgn):
+    r=getrtheory(theta0,ecc0,rad0,t)
+    y=sgn*np.sin(t)*r
+    return y
