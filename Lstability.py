e75121d38d31914261c9af304762c31c2c4f88f7
Steven Dorsher <sdorsher@gmail.com>
2020-11-25 16:45:22 -0600

    Updated E conservation and L conservation to handle unequal masses: not yet tested for all shapes and ranges of parameters

--- Lstability.py
+++ Lstability.py
@@ -1,11 +1,13 @@
 import numpy as np
 import math
 
-def Lstability(npstar1x,npstar2x,mass0):
+def Lstability(npstar1x,npstar2x,masses):
     npstar1pos=npstar1x[:,0:3]
     npstar1v=npstar1x[:,3:]
     npstar2pos=npstar2x[:,0:3]
     npstar2v=npstar2x[:,3:]
-    npstar1L=mass0*np.cross(npstar1v,npstar1pos-npstar2pos)
-    deltaL=np.abs((np.max(npstar1L[:,2])-np.min(npstar1L[:,2]))/np.mean(npstar1L[:,2]))
-    return deltaL, np.mean(npstar1L)
+    npstar1L=masses[0]*np.cross(npstar1v,npstar1pos)
+    npstar2L=masses[1]*np.cross(npstar2v,npstar2pos)
+    npstarLtot=npstar1L+npstar2L
+    deltaL=np.abs((np.max(npstarLtot[:,2])-np.min(npstarLtot[:,2]))/np.mean(npstarLtot[:,2]))
+    return deltaL, np.mean(npstarLtot)
