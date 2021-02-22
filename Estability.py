Fixed bug in RK4implicit doesnt need extra factor of h. Added Etot output and center of mass to E and L. E and L linearly decrease with t. Viral E oscialltes. Check convergence of E and L next. Checked for 2 body circular orbits. Same as for 2 star and 1 planet orbits.

Estability.py
@@ -6,8 +6,12 @@ def Estability(npstar1x,npstar2x,masses):
     npstar2pos=npstar2x[:,0:3]
     npstar2v=npstar2x[:,3:]
     nprstars12=np.sqrt(np.sum((npstar1pos-npstar2pos)**2,axis=1))
+    npcm=(masses[0]*npstar1pos+masses[1]*npstar2pos)/np.sum(masses)
+    npvcm =( masses[0]*npstar1v+masses[1]*npstar2v)/np.sum(masses)
+
+
     Egrav=-masses[0]*masses[1]/nprstars12
-    Ekinetic=masses[0]*0.5*np.sum(npstar1v**2,axis=1)+masses[1]*0.5*np.sum(npstar2v**2,axis=1)
+    Ekinetic=masses[0]*0.5*np.sum((npstar1v-npvcm)**2,axis=1)+masses[1]*0.5*np.sum((npstar2v-npvcm)**2,axis=1)
     Etot=Ekinetic+Egrav
     deltaE=np.abs(np.std(Etot)/np.mean(Etot))
-    return deltaE, np.mean(Etot)
+    return deltaE, np.mean(Etot),Etot,Ekinetic,Egrav

Lstability.py
@@ -6,8 +6,10 @@ def Lstability(npstar1x,npstar2x,masses):
     npstar1v=npstar1x[:,3:]
     npstar2pos=npstar2x[:,0:3]
     npstar2v=npstar2x[:,3:]
-    npstar1L=masses[0]*np.cross(npstar1v,npstar1pos)
-    npstar2L=masses[1]*np.cross(npstar2v,npstar2pos)
+    npcm=(masses[0]*npstar1pos+masses[1]*npstar2pos)/np.sum(masses)
+    npvcm=(masses[0]*npstar1v+masses[1]*npstar2v)/np.sum(masses)
+    npstar1L=masses[0]*np.cross(npstar1v-npvcm,npstar1pos-npcm)
+    npstar2L=masses[1]*np.cross(npstar2v-npvcm,npstar2pos-npcm)
     npstarLtot=npstar1L+npstar2L
     deltaL=np.abs((np.max(npstarLtot[:,2])-np.min(npstarLtot[:,2]))/np.mean(npstarLtot[:,2]))
-    return deltaL, np.mean(npstarLtot)
+    return deltaL, np.mean(npstarLtot),np.mean(npstarLtot), npstarLtot,npcm,npvcm